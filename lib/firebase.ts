e91ebd432c30acf23f276f9dfd6f93ea18c18f7b
krishnaT07 <160775556+krishnaT07@users.noreply.github.com>
2025-08-08 02:22:23 +0530

    added new file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  "projectId": "freelance-marketplace-a0f6b",
-  "appId": "1:286902035447:web:8d7e7ff002f25fd099c4f2",
-  "storageBucket": "freelance-marketplace-a0f6b.firebasestorage.app",
-  "apiKey": "AIzaSyA-NOSAYDBkpwi4bwM50I6VgPj5hiP8bSQ",
-  "authDomain": "freelance-marketplace-a0f6b.firebaseapp.com",
-  "messagingSenderId": "286902035447"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const storage = getStorage(app);
