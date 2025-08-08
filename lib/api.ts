f3d769734491dc4386faf888e723601c2b72d94e
krishnaT07 <160775556+krishnaT07@users.noreply.github.com>
2025-08-09 03:50:14 +0530

    Fix: api public url

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://freelancer-server-9l9n.onrender.com/api",
   withCredentials: true,
 });
 
